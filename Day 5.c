780c9bd03d3a2fb0e7fb5ae747ef52315e685630
Kavyarsb1 <kavyasingh822006@gmail.com>
2026-02-06 09:03:08 +0530

    Create Day 5.c
    Implement a program to merge and sort two logs of user arrival times.

---
+++ Day 5.c
@@ -0,0 +1,33 @@
+//Problem: A system receives two separate logs of user arrival times from two different servers. Each log is already sorted in ascending order. Your task is to create a single chronological log that preserves the correct order of arrivals.
+#include <stdio.h>
+int main(){
+    int a[100], b[100], n1, n2;
+
+    scanf("%d",&n1);
+    for(int i=0; i<n1; i++){
+        scanf("%d",&a[i]);
+    }
+    scanf("%d",&n2);
+    for(int i=0; i<n2; i++){
+        scanf("%d",&b[i]);
+    }
+
+    for(int i=0; i<n2; i++){
+        a[n1]=b[i];
+        n1++;
+    }
+    int temp;
+     for (int i = 0; i < n1 - 1; i++) {
+        for (int j = 0; j < n1 - i - 1; j++) {
+            if (a[j] > a[j + 1]) {
+                temp = a[j];
+                a[j] = a[j + 1];
+                a[j + 1] = temp;
+            }
+        }
+    }
+    for(int i=0; i<n1; i++){
+        printf("%d ",a[i]);
+    }
+
+}
